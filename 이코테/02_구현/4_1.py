a64ffd142842332244dc955e9ec89de17dc71d22
0woogie <80441481+0woogie@users.noreply.github.com>
2021-10-19 01:52:26 +0900

    Update 4_1.py

--- 이코테/02_구현/4_1.py
+++ 이코테/02_구현/4_1.py
@@ -1,1 +1,50 @@
 #상하좌우
+n = int(input())
+moves = input().split()
+x, y = 1, 1
+
+type = ['L', 'R', 'U', 'D']
+mx = [0, 0, -1, 1]
+my = [-1, 1, 0, 0]
+
+for move in moves:
+  for i in range(len(type)):
+    if move == type[i]:
+      nx = x + mx[i]
+      ny = y + my[i]
+  if nx<1 or nx>n or ny<1 or ny>n:
+    continue
+  x, y = nx, ny
+
+print(x, y)
+
+#맨 처음 구현한 소스코드. 이동타입('L','R','U','D')이 늘어날수록 소스코드가 길어짐, 리스트 활용을 잘 하자.
+"""
+n = int(input())
+move = input().split()
+x, y = 1, 1
+
+for m in move:
+  if m=='U':
+    if x==1:
+      continue
+    else:
+      x -= 1
+  elif m=='D':
+    if x==n:
+      continue
+    else:
+      x += 1
+  elif m=='L':
+    if y==1:
+      continue
+    else:
+      y -= 1
+  else:
+    if y==n:
+      continue
+    else:
+      y += 1
+
+print(x, y)
+"""
