cd740de31d06483d9dd996151af3832a98738f99
0woogie <80441481+0woogie@users.noreply.github.com>
2021-11-03 21:03:59 +0900

    Create 16_1.py
    'int' object is not callable 에러 발생 원인은 예약어를 변수명으로 사용했기 때문!
    해당 문제를 처음 풀 때 결과값을 나타내는 변수를 result가 아닌 max로 사용했더니
    test case 2에서 문제가 발생했다. 변수명을 통한 의미 전달도 중요하지만 예약어를 쓰지 않도록 유의하자.

---
+++ 이코테/06_DP/16_1.py
@@ -0,0 +1,30 @@
+#금광
+t = int(input())
+for _ in range(t):
+  n, m = map(int, input().split())
+  temp = list(map(int, input().split()))
+  count = 0
+  array = [[0]*m for _ in range(n)]
+  for i in range(n):
+    for j in range(m):
+      array[i][j] = temp[count]
+      count += 1
+  d = [[0]*m for _ in range(n)]
+  for i in range(n):
+    d[i][0] = array[i][0]
+
+  for j in range(1, m):
+    for i in range(n):
+      if i==0:
+        d[i][j] = max(d[i][j-1], d[i+1][j-1]) + array[i][j]
+      elif i==n-1:
+        d[i][j] = max(d[i][j-1], d[i-1][j-1]) + array[i][j]
+      else:
+        d[i][j] = max(d[i-1][j-1], d[i][j-1], d[i+1][j-1]) + array[i][j]
+
+  result = 0
+  for i in range(n):
+    if d[i][m-1]>result:
+      result = d[i][m-1]
+
+  print(result)
