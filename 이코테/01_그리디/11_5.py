9438fd53105130aa2e0545e7bcda78140ef7c473
0woogie <80441481+0woogie@users.noreply.github.com>
2022-01-21 15:55:15 +0900

    Update 11_5.py

--- 이코테/01_그리디/11_5.py
+++ 이코테/01_그리디/11_5.py
@@ -12,5 +12,17 @@ print(result)
 
 '''
 #개선된 풀이(이중루프 피하기, 최대 무게 m 활용하기)
-###
+n, m = map(int, input().split())
+balls = list(map(int, input().split()))
+result = 0
+
+weight_list = [0]*(m+1)
+for ball in balls:
+  weight_list[ball] += 1
+
+for i in range(1, m+1):
+  n -= weight_list[i]
+  result += weight_list[i]*n
+
+print(result)
 '''
