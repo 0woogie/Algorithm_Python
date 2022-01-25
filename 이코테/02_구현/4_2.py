b2c1e751a5fafd7e358d0c1e480a74650ba2a8a6
0woogie <80441481+0woogie@users.noreply.github.com>
2022-01-25 09:42:31 +0900

    Update 4_2.py

--- 이코테/02_구현/4_2.py
+++ 이코테/02_구현/4_2.py
@@ -5,11 +5,7 @@ count = 0
 for h in range(n+1):
   for m in range(60):
     for s in range(60):
-      if h%10==3:
-        count += 1
-      elif m//10==3 or m%10==3:
-        count += 1
-      elif s//10==3 or s%10==3:
+      if '3' in str(h)+str(m)+str(s):
         count += 1
 
 print(count)
