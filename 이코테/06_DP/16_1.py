644919ffd1ac95625a9077e45c5c0f242fa3ad11
0woogie <80441481+0woogie@users.noreply.github.com>
2022-02-24 17:13:50 +0900

    Update 16_1.py

--- 이코테/06_DP/16_1.py
+++ 이코테/06_DP/16_1.py
@@ -15,7 +15,7 @@ for _ in range(t):
 
   for j in range(1, m):
     for i in range(n):
-      if i==0:
+      if i==0: #재검토해보니 해당 코드의 경우 n=1일 때, 반드시 인덱스 오류가 발생한다. (이코테 답안을 참고하자)
         d[i][j] = max(d[i][j-1], d[i+1][j-1]) + array[i][j]
       elif i==n-1:
         d[i][j] = max(d[i][j-1], d[i-1][j-1]) + array[i][j]
