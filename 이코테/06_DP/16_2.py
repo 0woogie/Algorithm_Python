9734cb1594aed44521b7f7571b56b2af924919c0
0woogie <80441481+0woogie@users.noreply.github.com>
2022-02-24 18:03:22 +0900

    Update 16_2.py

--- 이코테/06_DP/16_2.py
+++ 이코테/06_DP/16_2.py
@@ -1,20 +1,17 @@
 #정수 삼각형
 n = int(input())
-array = [[0]*n for _ in range(n)]
+d = [[0]*n for _ in range(n)]
 
 for i in range(n):
   temp = list(map(int, input().split()))
   for j in range(len(temp)):
-    array[i][j] = temp[j]
+    d[i][j] = temp[j]
 
 for i in range(1, n):
   for j in range(i+1):
     if j==0:
-      array[i][j] += array[i-1][j]
+      d[i][j] += d[i-1][j]
     else:
-      array[i][j] += max(array[i-1][j-1], array[i-1][j])
+      d[i][j] += max(d[i-1][j-1], d[i-1][j])
 
-result = 0
-for i in range(n):
-  result = max(result, array[n-1][i])
-print(result)
+print(max(d[n-1]))
