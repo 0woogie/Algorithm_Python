adcafaefb34b98da857bf1dfc0e822b38189b4b5
0woogie <80441481+0woogie@users.noreply.github.com>
2022-03-03 23:17:03 +0900

    Update 5_1.py

--- 이코테/03_DFS&BFS/5_1.py
+++ 이코테/03_DFS&BFS/5_1.py
@@ -24,3 +24,29 @@ for i in range(n):
       result += 1
 
 print(result)
+
+'''
+#내 코드
+n, m = map(int, input().split())
+graph = []
+for _ in range(n):
+  graph.append(list(map(int, input())))
+
+def dfs(i, j):
+  if 0<=i<n and 0<=j<m:
+    if graph[i][j]==0:
+      graph[i][j] = 1
+      dfs(i-1, j)
+      dfs(i+1, j)
+      dfs(i, j-1)
+      dfs(i, j+1)
+  
+count = 0
+for i in range(n):
+  for j in range(m):
+    if graph[i][j]==0:
+      dfs(i, j)
+      count += 1
+
+print(count)
+'''
