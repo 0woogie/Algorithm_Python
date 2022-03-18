1d25faeaee18a4a010510e135f9673d2515cb514
0woogie <80441481+0woogie@users.noreply.github.com>
2022-03-18 16:02:22 +0900

    Update 8_4.py

--- 이코테/06_DP/8_4.py
+++ 이코테/06_DP/8_4.py
@@ -16,3 +16,24 @@ if d[m]==10001:
   print(-1)
 else:
   print(d[m])
+
+'''
+#다시 풀어봤을 때 내 풀이
+INF = int(1e9)
+n, m = map(int, input().split())
+money = []
+for _ in range(n):
+  money.append(int(input()))
+d = [INF]*(m+1)
+d[0] = 0
+for i in range(1, m+1):
+  for x in money:
+    if i-x<0:
+      continue
+    elif d[i-x]!=INF:
+      d[i] = min(d[i], d[i-x]+1)
+if d[m]==INF:
+  print(-1)
+else:
+  print(d[m])
+'''
