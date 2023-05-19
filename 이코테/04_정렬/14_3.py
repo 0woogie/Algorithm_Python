5501932cbc13bac0cd611f766b0142492dda4c85
0woogie <80441481+0woogie@users.noreply.github.com>
2023-05-19 11:40:08 +0900

    Update 14_3.py

--- 이코테/04_정렬/14_3.py
+++ 이코테/04_정렬/14_3.py
@@ -24,3 +24,22 @@ def solution(N, stages):
     answer = [x[0] for x in answer]
     
     return answer
+
+'''
+#다시 풀어봤을 때 풀이
+def solution(N, stages):
+    answer = []
+    array = [0]*(N+2)
+    for stage in stages:
+        array[stage] += 1
+    result = []
+    for i in range(1, N+1):
+        if array[i]==0: #ZeroDivisionError 에러가 발생할 수 있기 때문에 array[i]==0인 경우에는 따로 처리
+            result.append((i, 0))
+        else:
+            result.append((i, array[i]/sum(array[i:])))
+    result.sort(key = lambda x : (-x[1], x[0]))
+    for x in result:
+        answer.append(x[0])
+    return answer
+'''
